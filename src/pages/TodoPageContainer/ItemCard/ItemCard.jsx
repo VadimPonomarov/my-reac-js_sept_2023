4609c1c2b16b86b9c69b6f1a82a7c0ab9c7d1e5e
Вадим <pvs.versia@gmail.com>
2024-02-14 02:26:54 +0200

    hw-4

--- src/pages/TodoPageContainer/ItemCard/ItemCard.jsx
+++ src/pages/TodoPageContainer/ItemCard/ItemCard.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 
+import {motion} from "framer-motion";
+
 const ItemCard = ({props}) => {
     const {userId, id, title, completed} = props;
-    
+
     return (
-        <div className="card" style={{width: "18rem"}}>
+        <motion.div className="card" style={{width: "18rem"}}
+                    transition={{delay: Math.random()}}
+                    initial={{opacity: 0}}
+                    animate={{opacity: 1}}
+        >
             <div className="card-body">
                 <h5 className="card-title">{userId} - {id}</h5>
                 <p className="card-text">{title}</p>
                 <p className="card-text">{completed}</p>
             </div>
-        </div>
+        </motion.div>
     );
 };
 
