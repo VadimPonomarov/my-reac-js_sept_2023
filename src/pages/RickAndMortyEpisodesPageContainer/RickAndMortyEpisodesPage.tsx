0fcd723cfef1b3c4de200a84b3b88a7ff0caaa13
Вадим <pvs.versia@gmail.com>
2024-02-22 23:23:10 +0200

    hw-6

---
+++ src/pages/RickAndMortyEpisodesPageContainer/RickAndMortyEpisodesPage.tsx
@@ -0,0 +1,44 @@
+import * as React from "react";
+import {FC} from "react";
+
+import {Box, Button, ButtonGroup, Container, Stack} from "@mui/material";
+import {useAppEpisodesEffect} from "common/hooks/useAppEpisodesEffect";
+
+import {EpisodesButtonGroup} from "./EpisodesButtonGroup";
+import {EpisodesCard} from "./EpisodesCard";
+import styles from "./index.module.scss";
+
+const RickAndMortyEpisodesPage: FC = () => {
+    const {
+        info,
+        setInfo,
+        results,
+        setResults,
+        query,
+        setQuery
+    } = useAppEpisodesEffect()
+
+    const nextPage = () => {
+        setQuery({page: (+query.get("page") + 1).toString()})
+    }
+
+    const prevPage = () => {
+        setQuery({page: (+query.get("page") - 1).toString()})
+    }
+
+    return (
+        <div className={styles.rickAndMortyPage}>
+            <EpisodesButtonGroup props={{info, prevPage, nextPage}}/>
+
+            <Container sx={{display:"flex", flexWrap:"wrap", justifyContent:"center"}}>
+                {results.length &&
+                    results.map(episode =>
+                        <EpisodesCard props={{episode}}/>
+                    )
+                }
+            </Container>
+        </div>
+    );
+}
+
+export {RickAndMortyEpisodesPage}
